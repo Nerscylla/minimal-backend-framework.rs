ccc94c2dceff813319f958764e4f361dd91d8d85
TheBaum <the.baum12345@gmail.com>
2025-06-04 21:09:40 +0200

    refactor: request handling and improve error reporting

--- src/helpers/request_handler.rs
+++ src/helpers/request_handler.rs
@@ -1,32 +1,18 @@
-use std::{io::{Read, Write}, net::TcpStream};
+use std::{io::{Error, ErrorKind, Read, Write}, net::TcpStream};
 use crate::App;
 
 
 
 // hanlder function for streams coming in
 pub fn handle_incoming_stream(app: &mut App, mut stream: TcpStream) {
-    let buffer_size = app.buffer_size as usize;
-    let mut buffer = vec![0u8; buffer_size];
-
-    // Read up to buffer_size bytes
-    let bytes_read = match stream.read(&mut buffer) {
-        Ok(n) => n,
+    let request = match get_request_string(app, &mut stream) {
+        Ok(request) => request,
         Err(e) => {
-            eprintln!("Failed to read from stream: {}", e);
+            println!("Error getting Request: {}", e);
             return;
         }
     };
 
-    // If buffer is full, likely too large a request
-    if bytes_read == buffer_size {
-        eprintln!("Request too large, dropping connection");
-        let response = "HTTP/1.1 413 Payload Too Large\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
-        let _ = stream.write(response.as_bytes());
-        return;
-    }
-
-    // move buffer into string
-    let request = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
 
     // seperate the request into lines
     let request_lines: Vec<&str> = request.lines().collect();
@@ -39,6 +25,7 @@ pub fn handle_incoming_stream(app: &mut App, mut stream: TcpStream) {
             return;
         }
     };
+
 
     // split the first line up into words
     let split_first_line: Vec<String> = first_line
@@ -56,6 +43,7 @@ pub fn handle_incoming_stream(app: &mut App, mut stream: TcpStream) {
 
     // start building the response
     let response_text: String;
+
     // response function
     let response_function = app
         .routes
@@ -68,12 +56,14 @@ pub fn handle_incoming_stream(app: &mut App, mut stream: TcpStream) {
         // no handler
         response_text = "404 not found".to_string();
     }
+
     // put response into HTTP response
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {}\r\n\r\n{}",
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
         response_text.len(),
         response_text
     );
+
     // send back HTTP over tcp stream
     match stream.write_all(response.as_bytes()) {
         Ok(_) => {}
@@ -82,4 +72,33 @@ pub fn handle_incoming_stream(app: &mut App, mut stream: TcpStream) {
             return
         }
     }
+}
+
+
+
+// helper function to go from tcp stream to http request text
+fn get_request_string(app: &mut App, stream: &mut TcpStream) -> Result<String, Error> {
+	// create buffer
+	let buffer_size = app.buffer_size as usize;
+    let mut buffer = vec![0u8; buffer_size];
+
+    // Read up to buffer_size bytes
+    let bytes_read = match stream.read(&mut buffer) {
+        Ok(n) => n,
+        Err(e) => {
+            eprintln!("Failed to read from stream: {}", e);
+            return Err(e);
+        }
+    };
+
+    // If buffer is full, likely too large a request
+    if bytes_read == buffer_size {
+        eprintln!("Request too large, dropping connection");
+        let response = "HTTP/1.1 413 Payload Too Large\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+        let _ = stream.write(response.as_bytes());
+        return Err(Error::new(ErrorKind::Other, "Request too large"));
+    }
+
+    // move buffer into string and return
+    Ok(String::from_utf8_lossy(&buffer[..bytes_read]).to_string())
 }
